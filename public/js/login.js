2a68dbf1ad21c636f4286c66ac819b5067d2dd9c
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-01-15 16:44:07 -0800

    Separated login and signup js files

--- public/js/login.js
+++ public/js/login.js
@@ -24,33 +24,33 @@ const loginFormHandler = async (event) => {
   
 
 
-  // Sign Up
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
+  // // Sign Up
+  // const signupFormHandler = async (event) => {
+  //   event.preventDefault();
   
-    const username = document.querySelector('#username-input-signup').value.trim();
-    const password = document.querySelector('#password-input-signup').value.trim();
+  //   const username = document.querySelector('#username-input-signup').value.trim();
+  //   const password = document.querySelector('#password-input-signup').value.trim();
   
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+  //   if (username && password) {
+  //     const response = await fetch('/api/users', {
+  //       method: 'POST',
+  //       body: JSON.stringify({ username, password }),
+  //       headers: { 'Content-Type': 'application/json' },
+  //     });
   
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+  //     if (response.ok) {
+  //       document.location.replace('/dashboard');
+  //     } else {
+  //       alert(response.statusText);
+  //     }
+  //   }
+  // };
   
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  // document
+  //   .querySelector('.signup-form')
+  //   .addEventListener('submit', signupFormHandler);
   
