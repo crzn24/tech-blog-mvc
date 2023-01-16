1ee6eb715075b9157293c0c7151653317a1a7ea9
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-01-15 21:39:46 -0800

    deletes s at end of user in routes and changed . to # in scripts

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,7 +6,7 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector('#password-input-signup').value.trim();
   
     if (username && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/user', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
@@ -22,5 +22,5 @@ const signupFormHandler = async (event) => {
 
 
   document
-  .querySelector('.signup-form')
+  .querySelector('#signup-form')
   .addEventListener('submit', signupFormHandler);
