9a04b65a6b14654243dec03944b91ece81fc4987
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-01-17 18:29:10 -0800

    Tries to figure out comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,11 +1,11 @@
 const commentFormHandler = async function (event) {
     event.preventDefault();
 
-    const postId = document.querySelector('input[name="post-id"]').value;
-    const body = document.querySelector('textarea[name="comment-body"]').value;
+    const post_id = document.querySelector('input[name="post-id"]').value;
+    const comment_body = document.querySelector('textarea[name="comment-body"]').value;
 
     if (body) {
-        await fetch('/api/comment', {
+        await fetch('/api/comment/', {
             method: 'POST',
             body: JSON.stringify({
                 post_id,
